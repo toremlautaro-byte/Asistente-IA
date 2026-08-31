180c534e5fa949a780d4a12c0336472730ec1cec
toremlautaro-byte <toremlautaro@gmail.com>
2026-08-31 13:01:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE="mentor-v7-5";
+const CACHE="mentor-v7-6";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./mentor-icon.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET")return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
+});
+self.addEventListener("notificationclick",e=>{
+  e.notification.close();
+  e.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(list=>{
+    for(const c of list){if("focus" in c)return c.focus()}
+    if(clients.openWindow)return clients.openWindow("./");
+  }));
 });
